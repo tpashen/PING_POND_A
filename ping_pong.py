fa6c767bb2069bff8afe2068d41ce2a380fae2b7
Arrxeen <134214973+Arrxeen@users.noreply.github.com>
2023-05-28 15:55:34 +0200

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,17 +1,20 @@
 #1 
-from pygame import *
+from pygame import * 
+
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y,size_x, size_y, player_speed):
+    def __init__(self, player_image, player_y, player_speed,width,height):
         super().__init__()
 
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.image = transform.scale(image.load(player_image), (width, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
-        self.rect.x = player_x
+        self.rect.x =player_x
         self.rect.y = player_y
+
     def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+        window.blit(self.image, (self.rect.x , self.rect.y))
+
 
 #2
 class Player(GameSprite):
