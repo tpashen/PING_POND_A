3adf30b691beb8ebd62956d03beec6572def2fb7
alexeysupere <134214448+alexeysupere@users.noreply.github.com>
2023-05-28 17:03:22 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -42,3 +42,26 @@ while game:
         if sprite.collide_rect(racket1, ball) or sprite.collide_rect (racket2, ball):
             speed_x *= 1
             speed_y *= 1
+#4
+
+
+#5
+if ball.rect.y > win_height-50 or ball.rect.y < 0:
+    speed_y *= -1
+
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+        game_over = True
+
+
+    if ball.rect.x > win_width:
+        finish = True
+        window.blit(lose2,(200,200))
+        game_over = True
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+display.update()
+clock.tick(FPS)
